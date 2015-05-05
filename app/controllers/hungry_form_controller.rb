ddf4ad9f7036a1adb61b5b3355cb2e0dc99baf09
Andrey <andrey.bazhutkin@gmail.com>
2015-05-05 12:15:18 +1000

    Save form in cookies for a sake of simplicity. Disable turbolinks

--- app/controllers/hungry_form_controller.rb
+++ app/controllers/hungry_form_controller.rb
@@ -2,6 +2,7 @@ class HungryFormController < ApplicationController
   before_action :set_form, except: :submitted
 
   def show
+    @form.validate if params[:errors]
   end
 
   def update
@@ -14,9 +15,11 @@ class HungryFormController < ApplicationController
         render :show
       end
     when /prev/
+      save_form
       redirect_to hungry_form_path(@form.prev_page)
     when /submit/
       if @form.valid?
+        save_form
         redirect_to hungry_form_submitted_path
       else
         redirect_to hungry_form_path(@form.pages.find(&:invalid?), errors: true)
@@ -32,9 +35,17 @@ class HungryFormController < ApplicationController
   # Build a HungryForm::Form instance
   # See app/forms/sample_form.rb for definition
   def set_form
-    @form = SampleForm.build(params)
+    @form = SampleForm.build(params.merge(get_form))
   end
 
+  # A form can be stored anywhere as a hash
+  # For a sake of simplicity the form
+  # is stored in cookies
   def save_form
+    cookies[:form] = JSON.generate @form.values
+  end
+
+  def get_form
+    cookies[:form] ? JSON.parse(cookies[:form]) : {}
   end
 end
